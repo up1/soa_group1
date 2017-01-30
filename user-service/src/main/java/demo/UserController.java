92007a50cc89cffcb6f13ff3882f22c43f171a66
tharamalai <57070054@kmitl.ac.th>
2017-01-30 14:04:05 +0700

    initial user-service

---
+++ user-service/src/main/java/demo/UserController.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping("/user")
+    public User getUser(@RequestParam(value="id", defaultValue="1") int id) {
+        return this.userRepository.findById((long) id);
+    }
+}
