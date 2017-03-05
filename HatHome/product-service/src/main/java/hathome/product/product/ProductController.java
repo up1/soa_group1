71cf2039018819be3449147a9258837db5e634ee
Jessegade <s7070068@kmitl.ac.th>
2017-03-06 00:00:49 +0700

    connect database & edit controller, repository

--- HatHome/product-service/src/main/java/hathome/product/product/ProductController.java
+++ HatHome/product-service/src/main/java/hathome/product/product/ProductController.java
@@ -1,13 +1,25 @@
 package hathome.product.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+import java.util.List;
+
+@RestController()
 public class ProductController {
 
-    @GetMapping()
-    public void getAllProduct() {
+    private final ProductRepository productRepository;
 
+    @Autowired
+    public ProductController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @RequestMapping("/products")
+    public List<Product> getAllProduct(){
+        return this.productRepository.findAllProduct();
     }
 }
