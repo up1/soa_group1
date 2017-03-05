256180d6092466db27c0025a9f4661279af85415
amliomnie <mimiheadmeng@gmail.com>
2017-03-05 19:47:52 +0700

    try connect db

--- HatHome/authentication-service/src/main/java/hathome/authentication/authen/AuthenController.java
+++ HatHome/authentication-service/src/main/java/hathome/authentication/authen/AuthenController.java
@@ -1,13 +1,20 @@
 package hathome.authentication.authen;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/login")
+@RestController("/")
 public class AuthenController {
 
-    @GetMapping()
-    public void getAuthen() {
+    @GetMapping("/mimi")
+    public String getAuthen() {
+        return "mimi";
 
+    }
+
+    @RequestMapping("/hello")
+    public String sayHi(){
+        return "Hi";
     }
 }
