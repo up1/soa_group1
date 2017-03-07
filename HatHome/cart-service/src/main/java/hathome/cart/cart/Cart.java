0595d60d4c054b878c166fb3380467325b0961d4
Marktrs <marktrs69@gmail.com>
2017-03-07 12:04:39 +0700

    remove junk code

--- HatHome/cart-service/src/main/java/hathome/cart/cart/Cart.java
+++ HatHome/cart-service/src/main/java/hathome/cart/cart/Cart.java
@@ -7,16 +7,8 @@ public class Cart {
     private String id;
     private String name;
 
-    public String getId() {
-        return id;
-    }
-
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setName(String name) {
