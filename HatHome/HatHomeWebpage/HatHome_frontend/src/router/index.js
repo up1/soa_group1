3c4edc67a767ffeec5033b5cc67fa97c3276f114
Marktrs <marktrs69@gmail.com>
2017-03-19 20:29:08 +0700

    test route

--- HatHome/HatHomeWebpage/HatHome_frontend/src/router/index.js
+++ HatHome/HatHomeWebpage/HatHome_frontend/src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Mimi from '@/components/Mimi'
 
-Vue.use(Router)
-
-export default new Router({
+const router = new Router({
+  hashbang: false,
+  linkActiveClass: 'active',
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/mimi',
+      name: 'Mimi',
+      component: Mimi
     }
   ]
 })
+
+Vue.use(Router)
+Vue.router = router
+export default router
