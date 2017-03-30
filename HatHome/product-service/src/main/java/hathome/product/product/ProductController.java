5792b2c5de88fbd66ad17626491bd1a8412c6832
tharamalai <57070054@kmitl.ac.th>
2017-03-30 15:43:39 +0700

    fix cross origin

--- HatHome/product-service/src/main/java/hathome/product/product/ProductController.java
+++ HatHome/product-service/src/main/java/hathome/product/product/ProductController.java
@@ -1,14 +1,12 @@
 package hathome.product.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController()
+@CrossOrigin("http://localhost:8080")
 public class ProductController {
 
     private final ProductRepository productRepository;
