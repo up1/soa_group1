1854da89f91a16b1864177c68754316c4d5ae3d3
Marktrs <marktrs69@gmail.com>
2017-03-31 15:36:33 +0700

    import asset to main.js

--- HatHome/HatHomeWebpage/Hathome/src/main.js
+++ HatHome/HatHomeWebpage/Hathome/src/main.js
@@ -4,6 +4,25 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import './assets/css/animate.css'
+
+/*
+import './assets/css/bootstrap.min.css'
+import './assets/css/font-awesome.min.css'
+
+import './assets/css/price-range.css'
+import './assets/css/prettyPhoto.css'
+import './assets/css/responsive.css'
+
+*/
+/*
+import './assets/js/bootstrap.min.js'
+import './assets/js/html5shiv.js'
+import './assets/js/jquery.js'
+import './assets/js/main.js'
+import './assets/js/price-range.js'
+*/
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
