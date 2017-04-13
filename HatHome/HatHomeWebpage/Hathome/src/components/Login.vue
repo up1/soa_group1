f13e42297d7559cff90df764da8d74a3b7e71497
amliomnie <57070145@kmitl.ac.th>
2017-04-13 23:48:08 +0700

    try login page call auth api

---
+++ HatHome/HatHomeWebpage/Hathome/src/components/Login.vue
@@ -0,0 +1,72 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+        <div class="login-form">
+          <!--login form-->
+          <div class="loginForm">
+          <h2>{{header}}</h2>
+          <form method="POST">
+            <input type="email" placeholder="email" id="email" v-model="email"/>
+            <input type="password" placeholder="password" id="password" v-model="password" />
+            <div class="col-md-6">
+                                <span>
+                                <input type="checkbox" class="checkbox" id="checkbox" v-model="checked" > Keep me signed in
+                              </span>
+            </div>
+            <div class="col-md-6">
+                                <span>
+                                <a href="register.html"><p>Not a member yet?</p></a>
+                                </span>
+            </div>
+
+            <div class="row">
+              <div class="col-md-4"></div>
+              <div class="col-md-4"><button type="submit" class="btn btn-default" v-on:click="login">Login</button></div>
+              <div class="col-md-4"></div>
+            </div>
+          </form>
+          </div>
+        </div>
+        <!--/login form-->
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'loginForm',
+    data () {
+      return {
+        email: '',
+        password: '',
+        header: 'Login to your account',
+        checked:'',
+      }
+    },
+//    mounted: function () {
+//      this.login()
+//    },
+    methods: {
+      login: function () {
+          alert("mmmmm");
+        console.log('aaaaa')
+        axios.post('http://localhost:9002/login', {
+        email: this.email,
+        password: this.password
+          })
+          .then(function (response) {
+          console.log(response);
+        })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+
+    }
+  }
+</script>
--- HatHome/HatHomeWebpage/Hathome/src/router/index.js
+++ HatHome/HatHomeWebpage/Hathome/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  hashbang: false,
+  linkActiveClass: 'active',
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
