6db66f11c10e8b28082061c62b004045516b583a
amliomnie <57070145@kmitl.ac.th>
2017-04-14 13:32:40 +0700

    can redirect to homepage when login pass

--- HatHome/HatHomeWebpage/Hathome/src/components/Login.vue
+++ HatHome/HatHomeWebpage/Hathome/src/components/Login.vue
@@ -6,27 +6,29 @@
         <div class="login-form">
           <!--login form-->
           <div class="loginForm">
-          <h2>{{header}}</h2>
-          <form method="POST" v-on:submit.prevent="login()">
-            <input type="email" placeholder="email" id="email" v-model="email"/>
-            <input type="password" placeholder="password" id="password" v-model="password" />
-            <div class="col-md-6">
+            <h2>{{header}}</h2>
+            <form method="POST" v-on:submit.prevent="login()">
+              <input type="email" placeholder="email" id="email" v-model="email"/>
+              <input type="password" placeholder="password" id="password" v-model="password"/>
+              <div class="col-md-6">
                                 <span>
-                                <input type="checkbox" class="checkbox" id="checkbox" v-model="checked" > Keep me signed in
+                                <input type="checkbox" class="checkbox" id="checkbox" v-model="checked"> Keep me signed in
                               </span>
-            </div>
-            <div class="col-md-6">
+              </div>
+              <div class="col-md-6">
                                 <span>
                                 <a href="register.html"><p>Not a member yet?</p></a>
                                 </span>
-            </div>
+              </div>
 
-            <div class="row">
-              <div class="col-md-4"></div>
-              <div class="col-md-4"><button type="submit" class="btn btn-default" v-on:click="login">Login</button></div>
-              <div class="col-md-4"></div>
-            </div>
-          </form>
+              <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                  <button type="submit" class="btn btn-default" v-on:click="login">Login</button>
+                </div>
+                <div class="col-md-4"></div>
+              </div>
+            </form>
           </div>
         </div>
         <!--/login form-->
@@ -46,25 +48,26 @@
         email: '',
         password: '',
         header: 'Login to your account',
-        checked:'',
+        checked: '',
       }
     },
     methods: {
-      login: function (e) {
+      login(response) {
 //          e.preventDefault();
         axios.post('http://localhost:9002/login', {
-        email: this.email,
-        password: this.password
-          })
-          .then(function (response) {
-            this.$router.push({ path: '/' })
-              console.log(response);
+          email: this.email,
+          password: this.password
         })
-          .catch(function (error) {
-            console.log(error);
-          });
+          .then(
+            (response) => {
+              this.$router.push({path: '/'})
+              console.log(response);
+            },
+            () => {
+              console.log(error);
+            }
+          )
       }
-
     }
   }
 </script>
