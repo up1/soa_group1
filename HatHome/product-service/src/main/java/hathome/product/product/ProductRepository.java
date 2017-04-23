27b3c72a3c963d675aedd5f91152e143416483e8
Unknown <s7070068@kmitl.ac.th>
2017-04-23 21:19:58 +0700

    add pagination

--- HatHome/product-service/src/main/java/hathome/product/product/ProductRepository.java
+++ HatHome/product-service/src/main/java/hathome/product/product/ProductRepository.java
@@ -14,13 +14,11 @@ public class ProductRepository {
     private JdbcTemplate jdbcTemplate;
 
     public List<Product> findRecentProduct(){
-        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT WHERE amount!=0 ORDER BY id DESC LIMIT 12 ;", new ProductRowMapper());
+        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT WHERE amount!= 0 ORDER BY id DESC LIMIT 12;", new ProductRowMapper());
     }
 
-    public List<Product> findAllProductInPage(int page){
-        int item_per_page = 24;
-        int firstItem = (page-1) * item_per_page;
-        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT WHERE amount!=0 ORDER BY id LIMIT ?, ?", new Object[]{firstItem, item_per_page}, new ProductRowMapper());
+    public List<Product> findAllProduct(){
+        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT WHERE amount!= 0", new ProductRowMapper());
     }
 
     public Product findProductById(Long id){
