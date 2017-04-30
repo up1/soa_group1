f4a98d4fde2e2907304689a8f9e96b60d2027534
thanapa049 <s7070049@kmitl.ac.th>
2017-05-01 01:55:05 +0700

    add & delete wishlist
    but can not refresh page

--- HatHome/HatHomeWebpage/Hathome/src/components/ProductDetail.vue
+++ HatHome/HatHomeWebpage/Hathome/src/components/ProductDetail.vue
@@ -3,67 +3,66 @@
               <div class="row">
                   <div class="col-sm-3">
                       <div class="left-sidebar">
-                          <h2>Category</h2>
-                          <div class="panel-group category-products">
-                              <!--category-products-->
-                              <div class="cat-product checkbox">
-                                  <label><input type="checkbox" id="caps" value="Caps" v-model="checkedCat">Caps</label>
-                              </div>
-                              <div class="cat-product checkbox">
-                                  <label><input type="checkbox" id="hats" value="Hats" v-model="checkedCat">Hats</label>
-                              </div>
-                              <div class="cat-product checkbox">
-                                  <label><input type="checkbox" id="beanies" value="Beanies" v-model="checkedCat">Beanies</label>
-                              </div>
-
+                        <h2>Category</h2>
+                        <div class="panel-group category-products">
+                          <!--category-products-->
+                          <div class="cat-product checkbox">
+                            <label><input type="checkbox" id="caps" value="Caps" v-model="checkedCat">Caps</label>
                           </div>
-                          <!--/category-products-->
-
-                          <div class="brands_products">
-                              <!--brands_products-->
-                              <h2>Brands</h2>
-                              <div class="brands-name">
-                                  <div class="cat-product checkbox">
-                                      <label><input type="checkbox" id="newEra" value="newEra" v-model="checkedCat">New Era</label>
-                                  </div>
-                                  <div class="cat-product checkbox">
-                                      <label><input type="checkbox" id="converse" value="converse" v-model="checkedCat">Converse</label>
-                                  </div>
-                                  <div class="cat-product checkbox">
-                                      <label><input type="checkbox" id="jaxon" value="jaxon" v-model="checkedBrand">Jaxon</label>
-                                  </div>
-                              </div>
+                          <div class="cat-product checkbox">
+                            <label><input type="checkbox" id="hats" value="Hats" v-model="checkedCat">Hats</label>
                           </div>
-                          <!--/brands_products-->
-
-                          <div class="color_products">
-                              <!--color-->
-                              <h2>Color</h2>
-                              <div class="brands-name">
-                                  <div class="cat-product checkbox">
-                                      <label><input type="checkbox" id="black" value="black" v-model="checkedColor">black</label>
-                                  </div>
-                                  <div class="cat-product checkbox">
-                                      <label><input type="checkbox" id="white" value="white" v-model="checkedColor">white</label>
-                                  </div>
-                                  <div class="cat-product checkbox">
-                                      <label><input type="checkbox" id="red" value="red" v-model="checkedColor">red</label>
-                                  </div>
-                              </div>
+                          <div class="cat-product checkbox">
+                            <label><input type="checkbox" id="beanies" value="Beanies" v-model="checkedCat">Beanies</label>
                           </div>
-                          <!--/color-->
 
+                        </div>
+                        <!--/category-products-->
 
-                          <div class="price-range">
-                              <!--price-range-->
-                              <h2>Price Range</h2>
-                              <div class="price-slider">
-                                  <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2"><br />
-                                  <b>฿ 0</b> <b class="pull-right">฿ 600</b>
-                              </div>
+                        <div class="brands_products">
+                          <!--brands_products-->
+                          <h2>Brands</h2>
+                          <div class="brands-name">
+                            <div class="cat-product checkbox">
+                              <label><input type="checkbox" id="newEra" value="newEra" v-model="checkedCat">New Era</label>
+                            </div>
+                            <div class="cat-product checkbox">
+                              <label><input type="checkbox" id="converse" value="converse" v-model="checkedCat">Converse</label>
+                            </div>
+                            <div class="cat-product checkbox">
+                              <label><input type="checkbox" id="jaxon" value="jaxon" v-model="checkedBrand">Jaxon</label>
+                            </div>
                           </div>
-                          <!--/price-range-->
+                        </div>
+                        <!--/brands_products-->
 
+                        <div class="color_products">
+                          <!--color-->
+                          <h2>Color</h2>
+                          <div class="brands-name">
+                            <div class="cat-product checkbox">
+                              <label><input type="checkbox" id="black" value="black" v-model="checkedColor">black</label>
+                            </div>
+                            <div class="cat-product checkbox">
+                              <label><input type="checkbox" id="white" value="white" v-model="checkedColor">white</label>
+                            </div>
+                            <div class="cat-product checkbox">
+                              <label><input type="checkbox" id="red" value="red" v-model="checkedColor">red</label>
+                            </div>
+                          </div>
+                        </div>
+                        <!--/color-->
+
+
+                        <div class="price-range">
+                          <!--price-range-->
+                          <h2>Price Range</h2>
+                          <div class="price-slider">
+                            <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2"><br />
+                            <b>฿ 0</b> <b class="pull-right">฿ 600</b>
+                          </div>
+                        </div>
+                        <!--/price-range-->
 
 
                       </div>
@@ -86,30 +85,40 @@
                                   <p>{{detail}}</p>
 
                                   <p class="price-info"><b> {{price}} Baht</b></p>
-
-                                  <div style="text-align: left;">
-                                    <label>Quantity:</label>
+                                <div style="text-align: left;"><label>Quantity:</label></div>
+                                <div class="row">
+                                  <div class="col-sm-4" style="horiz-align: center; text-align: left;">
                                     <input type="text" value="1" />
-
-                                  <button type="button" class="btn btn-default cart" v-on:click="addToCart">
-                                    <i class="fa fa-shopping-cart"></i>
-                  										Add to cart
-                                  </button>
-                                  <button type="button" class="btn btn-default custom-button" v-on:click="addToWishlist(id, name)">
-                                    <i class="fa fa-star"></i>
-                                    Add To Wishlist
-                                  </button>
+                                  </div>
+                                  <div class="col-sm-4" style="horiz-align: center; text-align: left;">
+                                    <button type="button" class="btn btn-default cart" v-on:click="addToCart">
+                                      <i class="fa fa-shopping-cart"></i>
+                                      <!--Add to cart-->
+                                    </button>
+                                  </div>
+                                  <div v-if="wishlists_id.indexOf(id) < 0" class="col-sm-4" style="horiz-align: center; text-align: left;">
+                                    <button type="button" class="btn btn-default custom-button" v-on:click="addToWishlist(id)">
+                                      <i class="fa fa-star"></i>
+                                      <!--Add To Wishlist-->
+                                    </button>
+                                  </div>
+                                  <div v-else class="col-sm-4" style="horiz-align: center; text-align: left;">
+                                    <button type="button" class="btn wlclicked-button" v-on:click="deleteFromWishlist(id)">
+                                      <i class="fa fa-star"></i>
+                                      <!--Delete To Wishlist-->
+                                    </button>
+                                  </div>
+                                </div>
+                                <div style="text-align: left;">
                                   <p class="product-amount"><b>Amount:</b> {{amount}}</p>
                                   <p class="product-brand"><b>Brand:</b> {{brand}}</p>
                                   <p class="product-color"><b>Color:</b> {{color}}</p>
                                   <p class="product-cat"><b>Category:</b> {{type}}</p>
-                              </div>
+                                </div>
                               <!--/product-information-->
                           </div>
                       </div>
                       <!--/product-details-->
-
-
                   </div>
               </div>
           </div>
@@ -133,11 +142,14 @@
         brand: '',
         color: '',
         type: '',
-        id: ''
+        id: '',
+        wishlists: [],
+        wishlists_id: []
       }
     },
     mounted: function () {
-      this.product()
+      this.product();
+      this.getWishlist();
     },
     methods: {
       product: function () {
@@ -160,14 +172,30 @@
             console.log(error)
           })
       },
-      addToWishlist (id, name) {
-        wishlist.addToWishlist(id, name, this.$auth.user().id);
+      getWishlist: function () {
+        axios.get('http://localhost:9005/wishlist/user/' + this.$auth.user().id, {})
+          .then((response) => {
+            this.wishlists = response.data;
+            var i = 0;
+            for (i = 0; i < this.wishlists.length; i++) {
+              this.wishlists_id.push(this.wishlists[i].product_id);
+            }
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
       },
-      deleteFromWishlist (id){
+      addToWishlist (id) {
+        wishlist.addToWishlist(id, this.$auth.user().id);
+
+      },
+      deleteFromWishlist (productId){
         console.log(`DELETED`);
-        wishlist.deleteFromWishlist(id)
+        wishlist.deleteFromWishlist(productId, this.$auth.user().id)
           .then(() => {
-            this.wishlist()
+            console.log('aaaa');
+            this.wishlists = []
+            this.getWishlist()
           })
       },
       addToCart (){
