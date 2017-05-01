7956c153e98c5cd50d74f39049926cf144f2dcc8
Unknown <s7070068@kmitl.ac.th>
2017-05-02 03:03:12 +0700

    edit all file

--- HatHome/product-service/src/main/java/hathome/product/product/ProductRepository.java
+++ HatHome/product-service/src/main/java/hathome/product/product/ProductRepository.java
@@ -13,26 +13,26 @@ public class ProductRepository {
     private JdbcTemplate jdbcTemplate;
 
     public List<Product> findRecentProduct(){
-        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT WHERE amount!= 0 ORDER BY id DESC LIMIT 12;", new ProductRowMapper());
+        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM product WHERE amount!= 0 ORDER BY id DESC LIMIT 12;", new ProductRowMapper());
     }
 
     public List<Product> findAllProduct(){
-        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT WHERE amount!= 0", new ProductRowMapper());
+        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM product WHERE amount!= 0", new ProductRowMapper());
     }
 
     public Product findProductById(Long id){
-        return this.jdbcTemplate.queryForObject("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT WHERE id=?", new Object[]{id}, new ProductRowMapper());
+        return this.jdbcTemplate.queryForObject("SELECT id, name, detail, price, color, brand, amount, type FROM product WHERE id=?", new Object[]{id}, new ProductRowMapper());
     }
 
     public List<Product> findProductsInPage(int page){
         int itemPerPage = 9;
         int firstItem = (page-1) * itemPerPage;
-        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT WHERE amount!=0 ORDER BY id LIMIT ?, ?", new Object[]{firstItem, itemPerPage}, new ProductRowMapper());
+        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM product WHERE amount!=0 ORDER BY id LIMIT ?, ?", new Object[]{firstItem, itemPerPage}, new ProductRowMapper());
     }
 
     public List<Product> searchProduct(String keyword){
 
-        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM PRODUCT" +
+        return this.jdbcTemplate.query("SELECT id, name, detail, price, color, brand, amount, type FROM product" +
                 " WHERE (NAME LIKE '%" + keyword + "%'" +
                 " OR DETAIL LIKE '%" + keyword + "%'" +
                 " OR BRAND LIKE '%" + keyword + "%'" +
