468a23f066e963b0c1bb7a982834970f910226a7
Unknown <s7070068@kmitl.ac.th>
2017-05-01 20:36:43 +0700

    remove code smell model and adapter

--- HatHome/wishlist-service/src/main/java/hathome/wishlist/service/wishlist/Adapter/UserAdapter.java
+++ HatHome/wishlist-service/src/main/java/hathome/wishlist/service/wishlist/Adapter/UserAdapter.java
@@ -1,15 +1,11 @@
-package hathome.wishlist.service.wishlist.Adapter;
+package hathome.wishlist.service.wishlist.adapter;
 
 import org.springframework.web.client.RestTemplate;
 
-/**
- * Created by Plern on 24/4/2560.
- */
 public class UserAdapter {
     public User getUserById(long userId){
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:9001/user/" + userId;
-        User user = restTemplate.getForObject(url, User.class);
-        return user;
+        return restTemplate.getForObject(url, User.class);
     }
 }
