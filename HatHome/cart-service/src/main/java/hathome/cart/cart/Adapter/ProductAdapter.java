056fc8e755fd77fbf0bee9b66ec0261db016865b
Marktrs <marktrs69@gmail.com>
2017-05-08 14:41:52 +0700

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 510bd9616fb4fa749a8f1d311a62d060dab12056.

--- HatHome/cart-service/src/main/java/hathome/cart/cart/Adapter/ProductAdapter.java
+++ HatHome/cart-service/src/main/java/hathome/cart/cart/Adapter/ProductAdapter.java
@@ -3,9 +3,6 @@ package hathome.cart.cart.adapter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.net.Socket;
-
 /**
  * Created by Mark on 4/19/2017.
  */
@@ -13,17 +10,13 @@ import java.net.Socket;
 @Service
 public class ProductAdapter {
 
-    String ip = System.getProperty("128.199.126.179");
-    Socket socket = new Socket(ip, 9004);
-
-    public ProductAdapter() throws IOException {
-        //do nothing
-    }
-
+    String ip = "128.199.126.179";
 
     public Product getProductDetail(long productId){
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://"+ip+":9004/products/" + productId;
-        return  restTemplate.getForObject(url, Product.class);
+        Product productItem = restTemplate.getForObject(url, Product.class);
+
+        return  productItem;
     }
 }
