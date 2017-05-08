510bd9616fb4fa749a8f1d311a62d060dab12056
Marktrs <marktrs69@gmail.com>
2017-05-08 14:41:52 +0700

    Auto stash before merge of "master" and "origin/master"

--- HatHome/cart-service/src/main/java/hathome/cart/CartServiceApplication.java
+++ HatHome/cart-service/src/main/java/hathome/cart/CartServiceApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CartServiceApplication {
 
-	private CartServiceApplication(){
-		throw new IllegalAccessError("CartServiceApplication class");
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(CartServiceApplication.class, args);
 	}
--- HatHome/cart-service/src/main/java/hathome/cart/cart/Adapter/ProductAdapter.java
+++ HatHome/cart-service/src/main/java/hathome/cart/cart/Adapter/ProductAdapter.java
@@ -3,6 +3,9 @@ package hathome.cart.cart.adapter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Created by Mark on 4/19/2017.
  */
@@ -10,13 +13,17 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ProductAdapter {
 
-    String ip = "128.199.126.179";
+    String ip = System.getProperty("128.199.126.179");
+    Socket socket = new Socket(ip, 9004);
+
+    public ProductAdapter() throws IOException {
+        //do nothing
+    }
+
 
     public Product getProductDetail(long productId){
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://"+ip+":9004/products/" + productId;
-        Product productItem = restTemplate.getForObject(url, Product.class);
-
-        return  productItem;
+        return  restTemplate.getForObject(url, Product.class);
     }
 }
